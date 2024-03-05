99102c4dcfce077bae822cec74532e205feee733
osohyun0224 <osohyun0224@naver.com>
2024-03-05 10:54:14 +0900

    🐛  fix: 참조하는 url 경로 수정

--- playwright.config.js
+++ playwright.config.js
@@ -24,12 +24,10 @@ module.exports = defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+    baseURL: 'http://127.0.0.1:3000/vling',
     trace: 'on-first-retry',
   },
+
 
   /* Configure projects for major browsers */
   projects: [
